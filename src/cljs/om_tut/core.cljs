08ddcf11b39b04cb04fd89f1d1a01ba25a2e2589
magomimmo <mimmo.cosenza@gmail.com>
2014-01-24 01:29:58 +0100

    initial

---
+++ src/cljs/om_tut/core.cljs
@@ -0,0 +1,18 @@
+(ns om-tut.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(def app-state (atom {:text "Hello Wolrd!"}))
+
+(om/root
+  app-state
+  (fn [app owner]
+    (dom/h1 nil (:text app)))
+  (. js/document (getElementById "app")))
+
+
+
+
+
